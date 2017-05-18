fb56e3b440c4dc6727cb2eccc33c07776fe1932b
gyoungis <gyoungis@iastate.edu>
2017-05-17 20:00:04 -0500

    add test scene

---
+++ Assets/flashlight.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class flashlight : MonoBehaviour {
+
+    Light flashLight;
+    AudioSource soundSource;
+
+    void Start()
+    {
+        flashLight = GetComponent<Light>();
+        soundSource = GetComponent<AudioSource>();
+    }
+
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Mouse1))
+        {
+            if (flashLight.enabled == false)
+            {
+                flashLight.enabled = true;
+                soundSource.Play();
+            }
+            else
+            {
+                flashLight.enabled = false;
+                soundSource.Play();
+            }
+        }
+    }
+}
