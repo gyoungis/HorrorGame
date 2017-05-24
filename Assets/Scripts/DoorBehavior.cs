342f07ea5f0bb12bbc874b87d7d8808665d2d11e
brad boswell <bboswellff6@gmail.com>
2017-05-24 11:45:34 -0700

    added door stuff

---
+++ Assets/Scripts/DoorBehavior.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorBehavior : MonoBehaviour {
+
+    public bool open = false;
+    public float open_angle = 90f;
+    public float close_angle = 0f;
+    public float smooth = 2f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+
+    public void ChangeDoorState()
+    {
+        open = !open;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        if (open) {
+            Quaternion rot = Quaternion.Euler(0, open_angle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, rot, smooth * Time.deltaTime);
+        } else {
+            Quaternion rot = Quaternion.Euler(0, close_angle, 0);
+            transform.localRotation = Quaternion.Slerp(transform.localRotation, rot, smooth * Time.deltaTime);
+        }
+		
+	}
+}
---
+++ Assets/Scripts/InteractionBehavior.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractionBehavior : MonoBehaviour {
+
+    public float interact_distance = 5f;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (Input.GetKeyDown(KeyCode.Mouse0))
+        {
+            Ray ray = new Ray(transform.position, transform.forward);
+            RaycastHit hit;
+            if (Physics.Raycast(ray, out hit, interact_distance))
+            {
+                if (hit.collider.CompareTag("Door"))
+                {
+                    hit.collider.transform.GetComponent<DoorBehavior>().ChangeDoorState();
+                }
+            }
+        }
+	}
+}
