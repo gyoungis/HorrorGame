fe79cc182a0eee5b43bcd3aa06dbf8e3cacc2d7b
gyoungis <gyoungis@iastate.edu>
2017-06-06 12:40:42 -0500

    Fixed Raycasts

--- Assets/Scripts/InteractionBehavior.cs
+++ Assets/Scripts/InteractionBehavior.cs
@@ -8,9 +8,9 @@ public class InteractionBehavior : MonoBehaviour {
     private Camera fpsCam;
 
     // Debugging
-    public GameObject hitMarker;
-    private LineRenderer hitLine;
-    private WaitForSeconds Duration = new WaitForSeconds(0.07f);
+    //public GameObject hitMarker;
+    //private LineRenderer hitLine;
+    //private WaitForSeconds Duration = new WaitForSeconds(0.07f);
 
     //TODO: Don't do this here
     AudioSource[] audio;
@@ -22,7 +22,7 @@ public class InteractionBehavior : MonoBehaviour {
         audio = GetComponentsInChildren<AudioSource>();
 
         // Debug
-        hitLine = GetComponent<LineRenderer>();
+        //hitLine = GetComponent<LineRenderer>();
 
     }
 
@@ -30,68 +30,64 @@ public class InteractionBehavior : MonoBehaviour {
     void Update () {
         if (Input.GetKeyDown(KeyCode.E))
         {
-            StartCoroutine(lineEffect());
 
+            //Debugging
+            //StartCoroutine(lineEffect());
+            //hitLine.SetPosition(0, rayOrigin);
 
-            //Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, fpsCam.nearClipPlane));
-            Vector3 rayOrigin = fpsCam.transform.position;
-            hitLine.SetPosition(0, rayOrigin);
-            Ray ray = fpsCam.ViewportPointToRay(new Vector3(0.5f, 0.5f, 0.7f));
+            Vector3 rayOrigin = fpsCam.ViewportToWorldPoint(new Vector3(0.5f, 0.5f, 0));
+            
             RaycastHit hit;
-            Debug.Log(rayOrigin);
-            //Ray ray = new Ray(transform.position, transform.forward);
-            //if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, interact_distance))
-            if (Physics.Raycast(ray, out hit))
-            {
-                
-                hitLine.SetPosition(1, hit.point);
-                //if (hit.collider.CompareTag("Door"))
-                //{
-                //    hit.collider.transform.GetComponent<DoorBehavior>().ChangeDoorState();
-                //    hit.collider.transform.GetComponent<DoorBehavior>().PlaySound();
-                //    Instantiate(hitMarker, hit.point, Quaternion.identity);
-                //}
-                //if (hit.collider.CompareTag("Note"))
-                //{
-                //    Note state = hit.collider.GetComponent<Note>();
-                //    Instantiate(hitMarker, hit.point, Quaternion.identity);
-                //    audio[4].Play();
-                //    if (state != null)
-                //    {
-                //        state.pickedUp(1);
-                //        //state.transform.position = Camera.main.transform.position + Camera.main.transform.forward * 2.0f;
-                //    }
-                //}
 
-                DoorBehavior door = hit.collider.GetComponent<DoorBehavior>();
-                if (door != null)
+            Debug.Log(rayOrigin);
+
+            if (Physics.Raycast(rayOrigin, fpsCam.transform.forward, out hit, interact_distance))
+            {
+                //Debugging
+                //hitLine.SetPosition(1, hit.point);
+
+                if (hit.collider.CompareTag("Door"))
                 {
                     hit.collider.transform.GetComponent<DoorBehavior>().ChangeDoorState();
                     hit.collider.transform.GetComponent<DoorBehavior>().PlaySound();
-                    Instantiate(hitMarker, hit.point, Quaternion.identity);
+
+                    //Debugging
+                    //Instantiate(hitMarker, hit.point, Quaternion.identity);
+                }
+                if (hit.collider.CompareTag("Note"))
+                {
+
+                    Note state = hit.collider.GetComponent<Note>();
+                    
+                    audio[4].Play();
+                    if (state != null)
+                    {
+                        state.pickedUp(1);
+                    }
+
+                    //Debugging
+                    //Instantiate(hitMarker, hit.point, Quaternion.identity);
                 }
 
-                Note hitNote = hit.collider.GetComponent<Note>();
-                if (hitNote != null)
-                {
-                    Instantiate(hitMarker, hit.point, Quaternion.identity);
-                    audio[4].Play();
-                    hitNote.pickedUp(1);
-                }
+
             }
-            else
-            {
-                hitLine.SetPosition(1, fpsCam.transform.forward * interact_distance);
-            }
+
+            // Debugging Raycasts
+            //else
+            //{
+            //    hitLine.SetPosition(1, fpsCam.transform.forward * interact_distance);
+            //}
         }
 	}
 
-    private IEnumerator lineEffect()
-    {
-        hitLine.enabled = true;
 
-        yield return Duration;
+    // Debug line render
+    //private IEnumerator lineEffect()
+    //{
+    //    hitLine.enabled = true;
 
-        hitLine.enabled = false;
-    }
+    //    yield return Duration;
+
+    //    hitLine.enabled = false;
+    //}
 }
